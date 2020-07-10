1540317001aa85d2ae71009dba6b7d0cd220f7f8
Awet <awetkifle94@gmail.com>
2020-07-10 23:33:36 +0100

    add test for booking add

--- src/__tests__/bookingReducer.test.js
+++ src/__tests__/bookingReducer.test.js
@@ -1,8 +1,19 @@
 import reducer from "../redux/reducer";
-
-describe("booking reducer", () => {
-  it("should return atleast one booking initally", () => {
-    const result = reducer([{ firstname: "awet" }], { action: "anything" });
-    expect(result).toHaveLength(1);
+import data from "../data/data.json";
+import { BOOKING_ADD } from "../redux/actions";
+describe("bookings reducer", () => {
+  const INITAL_VALUE = data;
+  it("should return all bookings initally", () => {
+    const result = reducer(INITAL_VALUE, { action: "anything" });
+    expect(result).toHaveLength(2);
+  });
+  it("should add new booking", () => {
+    const newBooking = { firstname: "John" };
+    const result = reducer(INITAL_VALUE, {
+      type: BOOKING_ADD,
+      booking: newBooking,
+    });
+    expect(result).toHaveLength(3);
+    expect(result[2].firstname).toEqual("John");
   });
 });
