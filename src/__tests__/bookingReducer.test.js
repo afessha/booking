c27e8bf1e194ba94e91799611d14f32147304889
Awet <awetkifle94@gmail.com>
2020-07-10 23:40:36 +0100

    test for mark seated

--- src/__tests__/bookingReducer.test.js
+++ src/__tests__/bookingReducer.test.js
@@ -1,6 +1,6 @@
 import reducer from "../redux/reducer";
 import data from "../data/data.json";
-import { BOOKING_ADD } from "../redux/actions";
+import { BOOKING_ADD, BOOKING_MARK_SEATED } from "../redux/actions";
 describe("bookings reducer", () => {
   const INITAL_VALUE = data;
   it("should return all bookings initally", () => {
@@ -15,5 +15,13 @@ describe("bookings reducer", () => {
     });
     expect(result).toHaveLength(3);
     expect(result[2].firstname).toEqual("John");
+  });
+  it("should mark booking as seated", () => {
+    const result = reducer(INITAL_VALUE, {
+      type: BOOKING_MARK_SEATED,
+      bookingId: "1594293894023",
+      value: "Seated",
+    });
+    expect(result[0].status).toEqual("Seated");
   });
 });
