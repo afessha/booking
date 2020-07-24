4a2730578d1bf7e2fca9eb088180f3afc58d5d45
Awet <awetkifle94@gmail.com>
2020-07-24 12:39:12 +0100

    use combinereducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,25 +1,8 @@
-import { BOOKING_ADD, BOOKING_MARK_SEATED, BOOKING_LOADED } from "./actions";
+import { combineReducers } from "redux";
+import bookingReducer from "./bookingrReducer";
 
-const INITAL_BOOKINGS = [];
+const reducer = combineReducers({
+  bookings: bookingReducer,
+});
 
-function bookingReducer(state = INITAL_BOOKINGS, action) {
-  switch (action.type) {
-    case BOOKING_LOADED:
-      return [...state, action.initalBookings];
-    case BOOKING_ADD:
-      return [...state, action.booking];
-    case BOOKING_MARK_SEATED:
-      return state.map((booking) => {
-        if (booking.id === action.bookingId) {
-          return {
-            ...booking,
-            status: action.value,
-          };
-        } else return booking;
-      });
-    default:
-      return state;
-  }
-}
-
-export default bookingReducer;
+export default reducer;
