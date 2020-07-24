10fe6fedf4fbf585203aa18ab20459bff5f12a4e
Awet <awetkifle94@gmail.com>
2020-07-24 12:39:44 +0100

    add redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import bookingReducer from "./reducer";
+import reducer from "./reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 const initialState = [];
 const middleware = [thunk];
-
-const store = createStore(
-  bookingReducer,
+let store = createStore(
+  persistedReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
-export default store;
+let persistor = persistStore(store);
+export { store, persistor };
