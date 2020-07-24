f752b22283fc0ab25e5eb0a222bed9d1cfa0320f
Awet <awetkifle94@gmail.com>
2020-07-24 12:35:37 +0100

    change route

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import Booking from "./components/booking/Booking";
 import Seating from "./components/seating/Seating";
 import "./App.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header className="header-link">
-          <Link className="link-element" to="/booking">
+          <Link className="link-element" to="/">
             Booking
           </Link>
-          <Link className="link-element" to="/booking/booking-list">
+          <Link className="link-element" to="/booking-list">
             Seating
           </Link>
         </header>
-        <Route exact={true} path="/booking" component={Booking} />
-        <Route path="/booking/booking-list" component={Seating} />
+        <Route exact={true} path="/" component={Booking} />
+        <Route path="/booking-list" component={Seating} />
       </div>
     </BrowserRouter>
   );
