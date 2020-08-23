08b9ac7ed2448fc1642deff1df6e5f8a5f58ce6c
Awet <awetkifle94@gmail.com>
2020-08-23 23:57:17 +0100

    update tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import { create } from "react-test-renderer";
-import Booking from "../components/booking/Booking";
-import Seating from "../components/seating/Seating";
 import { BrowserRouter, Route } from "react-router-dom";
 import App from "../App";
 
 describe("App component", () => {
   test("Matches the snapshot", () => {
     const app = create(<App />);
-    expect(app.toJSON()).toMatchSnapshot();
+    expect(app).toMatchSnapshot();
   });
 });
