203a60500d649d2bdb3db907c8c55755c1c33970
Awet <awetkifle94@gmail.com>
2020-09-15 22:34:59 +0100

    add build

---
+++ build/precache-manifest.c7bcdd6bbe8a1f0e5c5a50ddb844120a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef844569e6f81f58af9b85ea8b9a2af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c77dda0b0f0393a491",
+    "url": "/static/css/main.39a9f200.chunk.css"
+  },
+  {
+    "revision": "7780c40e3540c0a6dc0c",
+    "url": "/static/js/2.9e2a2825.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.9e2a2825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c77dda0b0f0393a491",
+    "url": "/static/js/main.876371f5.chunk.js"
+  },
+  {
+    "revision": "c3c2a674d3b0dbf517ab",
+    "url": "/static/js/runtime-main.d4e443d4.js"
+  }
+]);
