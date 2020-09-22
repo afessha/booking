7929e139b0103ff5c12db1d4d94e41053f951af1
Awet <awetkifle94@gmail.com>
2020-09-22 23:47:08 +0100

    update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../booking/Booking.css";
 const Footer = () => {
-  return <div className="footer"> Awet Fessha @2020 </div>;
+  return <div className="footer"> Awet Fessha ©2020 </div>;
 };
 export default Footer;
